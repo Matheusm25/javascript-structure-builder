07bb3ea859f299af790d2017e52b53bb3824b2f3
Matheus Marques <matheusml25@hotmail.com>
2020-01-27 12:19:31 -0300

    Generate react pages created

---
+++ src/commands/generate:rp.ts
@@ -0,0 +1,25 @@
+/**
+ * Create a react page with a index.js and a index.css file
+ */
+module.exports = {
+  name: 'generate:rp',
+  alias: ['grp'],
+  description: 'Create new page inside src/pages',
+  run: async toolbox => {
+    const { parameters, createComponent, filesystem, wantOverwrite } = toolbox;
+
+    const name = parameters.first;
+
+    if (
+      filesystem.exists(
+        `src${filesystem.separator}pages${filesystem.separator}${name}`
+      )
+    ) {
+      if (!(await wantOverwrite(name))) {
+        return;
+      }
+    }
+
+    await createComponent(`src${toolbox.filesystem.separator}pages`, name);
+  }
+};
